6f3d215241ef1abb997fa223361dc50c0d788703
Ivan Antunes <ivanantnes75@gmail.com>
2023-09-27 20:38:24 -0300

    init header and add imgs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
+// ! Angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// ! Routing
+import { AppRoutingModule } from './app-routing.module';
+
+// ! Components
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
