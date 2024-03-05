1ab5e7a3e6be9f1a68045d9d8c229a3b39dc06a3
Ivan Antunes <ivanantnes75@gmail.com>
2024-03-05 20:54:48 -0300

    reafactory loading

--- src/app/components/loading/loading.component.ts
+++ src/app/components/loading/loading.component.ts
@@ -3,11 +3,26 @@ import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @Component({
     selector: 'app-loading',
-    templateUrl: './loading.component.html',
-    styleUrls: ['./loading.component.scss'],
+    template: `
+      <section class="loading">
+        <mat-spinner color="warn"></mat-spinner>
+      </section>
+    `,
+    styles: `
+      .loading {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        background: rgba(0, 0, 0, 0.15);
+        z-index: 200;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+    `,
     standalone: true,
     imports: [MatProgressSpinner]
 })
-export class LoadingComponent {
-
-}
+export class LoadingComponent { }
